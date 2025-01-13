fd8f91d343536f844e1c8bea8cad1f520b0a814b
mirziyo <mirziyo>
2025-01-13 22:46:31 +0500

    Add initial HTML structure, CSS styles, and JavaScript functionality

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// const clicks = document.querySelector("#click");
+// const fonar = document.querySelector(".fonar-image");
+
+// clicks.addEventListener("click", function () {
+//   fonar.classList.click("on");
+// });
+
+fonaric.addEventListener("click", function () {
+  fonar.classList.click("on");
+  if (fonar.classList)
+});
